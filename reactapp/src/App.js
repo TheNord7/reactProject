db6c28fa9427c05af4677ec24228ac318a248dcf
TheNord7 <jorawrx@gmail.com>
2022-04-07 21:56:13 +0300

    lesson 3

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,37 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import Message from './Message';
 import Authors from './authors';
+import { Button } from '@mui/material';
+import { Send } from '@mui/icons-material';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MessageList from './MessageList';
+
 
 
 function App(props) {
+
   const [messageList, setmessageList] = useState([]);
   const [value, setValue] = useState('');
+  const inputRef = useRef(null);
 
   const changeText = (event) => {
     setValue(event.target.value)
   }
 
-  const addText = () => {
+  const addText = (event) => {
+    event.preventDefault();
     setmessageList([...messageList, { text: value, author: Authors.user }])
+    setValue('')
+    inputRef.current?.focus();
   }
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [])
 
   useEffect(() => {
     if (messageList.length > 0 && messageList[messageList.length - 1].author !== Authors.bot) {
@@ -26,14 +42,45 @@ function App(props) {
     }
   }, [messageList])
 
+  const sendForm = (event) => {
+    event.preventDefault()
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <span>Чат:</span><br />
-        {messageList.map(el => (<Message text={el} />))}
+
         <div>
-          <input value={value} onChange={changeText} />
-          <button onClick={addText}>Отправить</button>
+
+          <div className='chatWrap'>
+            <MessageList />
+            <form onSubmit={sendForm}>
+              <span>Чат:</span><br /><br />
+              {messageList.map((el, index) => (<Message text={el} file={index} />))}
+              <Input
+                style={{ marginRight: '10px' }}
+                inputRef={inputRef}
+                value={value} onChange={changeText}
+                id="input-with-icon-adornment"
+                startAdornment={
+                  <InputAdornment position="start" color="#fff">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+              <Button style={{ color: 'white' }} type='submit' variant="contained" disableElevation onClick={addText}>
+                Send
+                <Send style={{ marginLeft: '10px' }} />
+              </Button>
+
+            </form>
+
+
+          </div>
+
         </div>
       </header>
     </div >
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
 
-const word = "react";
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  status: {
+    danger: orange[500],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={word} />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
